943485391fbf8cbe267e1beaeb20900ce60b98b8
giuseppecantafio <giuseppecantafio@hotmail.it>
2022-04-20 16:21:53 +0200

    aggiunto bonus

--- js/script.js
+++ js/script.js
@@ -5,12 +5,14 @@ const app = new Vue({
         nome: '',
         immagine: 'https://i.picsum.photos/id/266/200/300.jpg?hmac=nAZYC6vsnq4fuOzfge00Ylvi9ALRMbMA8wxBxIPTjs0',
         isHidden: true,
+        isNoActive: false,
 
     },
     methods: {
         bottoneCliccato(nome) {
             this.titolo = nome;
             this.isHidden = false;
+            this.isNoActive = true;
             return nome;
 
         }

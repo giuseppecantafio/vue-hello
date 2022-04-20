219776e378db8c3bdfe979cbd821ec66311b1986
giuseppecantafio <giuseppecantafio@hotmail.it>
2022-04-20 16:05:50 +0200

    esercizio concluso, refactory in corso

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        titolo: '',
+        nome: '',
+        immagine: 'https://i.picsum.photos/id/266/200/300.jpg?hmac=nAZYC6vsnq4fuOzfge00Ylvi9ALRMbMA8wxBxIPTjs0',
+        isHidden: true,
+
+    },
+    methods: {
+        bottoneCliccato(nome) {
+            this.titolo = nome;
+            this.isHidden = false;
+            return nome;
+
+        }
+    }, 
+})
